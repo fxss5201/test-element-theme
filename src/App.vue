3f67459574aac087c97bf151e872743670254f17
樊玉顺 <fooww-fanyushun@example.com>
2019-06-27 13:24:07 +0800

    github例子

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
       <router-link to="/about">About</router-link>
     </div>
     <el-row>
+      <a class="theme-a red" href="/test-element-theme/?theme=red">红色主题色</a>
+      <a class="theme-a green" href="/test-element-theme/?theme=green">绿色主题色</a>
+      <a class="theme-a blue" href="/test-element-theme/?theme=blue">蓝色主题色</a>
+    </el-row>
+    <el-row>
       <el-button type="primary">主要按钮</el-button>
       <el-button type="primary" plain>主要按钮</el-button>
       <el-button type="primary" round>主要按钮</el-button>
@@ -29,6 +34,19 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+.theme-a {
+  margin: 20px;
+  line-height: 50px;
+  &.red {
+    color: red;
+  }
+  &.green {
+    color: green;
+  }
+  &.blue {
+    color: blue;
   }
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  publicPath: '/test-element-theme/',
+  outputDir: 'docs'
+}
