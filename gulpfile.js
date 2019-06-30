9463929d6771567678e408c09cfa932c92c195ee
fys <723107234@qq.com>
2019-06-30 16:49:20 +0800

    1、采用 gulp4 的语法重构了 gulpfile.js 文件 2、修改 README.md 文档 3、重新编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,79 @@
+/* eslint-disable camelcase */
 'use strict'
 
-const gulp = require('gulp')
+const { src, dest, watch, task, series, parallel } = require('gulp')
 const less = require('gulp-less')
 const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
 const concat = require('gulp-concat')
 const rename = require('gulp-rename')
 
-// 开发辅助环境
-function buildRed () {
-  return gulp.src('./src/style/red.less')
+let buildSrcStyle_path
+// 将 src/style 中的 less 文件编译为 css 文件，并将生成文件输出到对应的 theme 文件夹下
+function buildSrcStyle () {
+  const currentPath = buildSrcStyle_path || '*'
+  return src(`./src/style/${currentPath}.less`)
     .pipe(less())
     .pipe(autoprefixer())
     .pipe(cssmin())
-    .pipe(gulp.dest('./theme/red'))
+    .pipe(dest(function (file) {
+      return `./theme/${file.stem}`
+    }))
 }
 
-function concatRed () {
-  return gulp.src(['./theme/red/index.css', './theme/red/red.css'])
-    .pipe(concat('index.css'))
-    .pipe(cssmin())
-    .pipe(rename({ basename: 'index', suffix: '.min' }))
-    .pipe(gulp.dest('./theme/red'))
+task('buildSrcStyle', series(buildSrcStyle))
+
+let themeConcat_path
+// 将 theme 文件夹下某个主题色中的 css 文件合并
+function themeConcat () {
+  if (!themeConcat_path) {
+    return false
+  } else {
+    return src([`./theme/${themeConcat_path}/index.css`, `./theme/${themeConcat_path}/${themeConcat_path}.css`])
+      .pipe(concat('index.css'))
+      .pipe(cssmin())
+      .pipe(rename({ basename: 'index', suffix: '.min' }))
+      .pipe(dest(`./theme/${themeConcat_path}`))
+  }
 }
 
-function buildBlue () {
-  return gulp.src('./src/style/blue.less')
-    .pipe(less())
-    .pipe(autoprefixer())
-    .pipe(cssmin())
-    .pipe(gulp.dest('./theme/blue/'))
-}
+task('themeConcat', series(themeConcat))
 
-function concatBlue () {
-  return gulp.src(['./theme/blue/index.css', './theme/blue/blue.css'])
-    .pipe(concat('index.css'))
-    .pipe(cssmin())
-    .pipe(rename({ basename: 'index', suffix: '.min' }))
-    .pipe(gulp.dest('./theme/blue'))
-}
+task('watch', function () {
+  const watcher = watch(['./theme/**/index.css', 'src/style/*.less'])
+  const needWatcher = 'red'
+  const watcherPath = watch(`./theme/**/${needWatcher}.css`)
 
-function buildGreen () {
-  return gulp.src('./src/style/green.less')
-    .pipe(less())
-    .pipe(autoprefixer())
-    .pipe(cssmin())
-    .pipe(gulp.dest('./theme/green/'))
-}
+  watcher.on('change', function (path) {
+    if (path.indexOf('.less') > -1) {
+      buildSrcStyle_path = path.split('\\')[2].split('.')[0]
+      buildSrcStyle()
+    } else {
+      themeConcat_path = path.split('\\')[1]
+      themeConcat()
+    }
+  })
 
-function concatGreen () {
-  return gulp.src(['./theme/green/index.css', './theme/green/green.css'])
-    .pipe(concat('index.css'))
-    .pipe(cssmin())
-    .pipe(rename({ basename: 'index', suffix: '.min' }))
-    .pipe(gulp.dest('./theme/green'))
-}
-
-gulp.task('watch', function () {
-  gulp.watch(['./theme/**/index.css', 'src/style/*.less'], gulp.parallel(
-    gulp.series(buildRed, concatRed),
-    gulp.series(buildBlue, concatBlue),
-    gulp.series(buildGreen, concatGreen)))
+  watcherPath.on('change', function (path) {
+    themeConcat_path = path.split('\\')[1]
+    themeConcat()
+  })
 })
 
 // 将文件移动到对应的 public 目录
-
+let fonts_path = 'red'
 function moveFontsFile () {
-  return gulp.src('./theme/red/fonts/**').pipe(gulp.dest('./public/theme/fonts'))
+  return src(`./theme/${fonts_path}/fonts/**`).pipe(dest('./public/theme/fonts'))
 }
 
-function moveRedFile () {
-  return gulp.src('./theme/red/index.min.css')
-    .pipe(rename({ basename: 'red.min' }))
-    .pipe(gulp.dest('./public/theme'))
+let move_path = ['red', 'green', 'blue']
+function moveFile (cb) {
+  move_path.forEach(element => {
+    return src(`./theme/${element}/index.min.css`)
+      .pipe(rename({ basename: `${element}.min` }))
+      .pipe(dest('./public/theme'))
+  })
+  cb()
 }
 
-function moveBlueFile () {
-  return gulp.src('./theme/blue/index.min.css')
-    .pipe(rename({ basename: 'blue.min' }))
-    .pipe(gulp.dest('./public/theme'))
-}
-
-function moveGreenFile () {
-  return gulp.src('./theme/green/index.min.css')
-    .pipe(rename({ basename: 'green.min' }))
-    .pipe(gulp.dest('./public/theme'))
-}
-
-gulp.task('build', gulp.parallel(
-  gulp.series(buildRed, concatRed, moveFontsFile, moveRedFile),
-  gulp.series(buildBlue, concatBlue, moveBlueFile),
-  gulp.series(buildGreen, concatGreen, moveGreenFile))
-)
+task('build', parallel(moveFontsFile, moveFile))
